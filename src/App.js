48ce5bf5a693a9f67f6e6abb17e6bad17fbeffe6
Nguyen Quoc An <an567008@gmail.com>
2022-05-30 20:00:14 +0700

    build default layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { publicRoutes } from './routes';
+// Layout
+import DefaultLayout from './layout/DefaultLayout';
 
 const App = () => {
-  return <div>App</div>;
+    return (
+        <Routes>
+            {publicRoutes.map((route, index) => {
+                const Page = route.component;
+
+                let Layout;
+
+                if (route.layout) {
+                    Layout = route.layout;
+                } else if (route.layout === null) {
+                    Layout = Fragment;
+                } else {
+                    Layout = DefaultLayout;
+                }
+
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                            <Layout>
+                                <Page />
+                            </Layout>
+                        }
+                    />
+                );
+            })}
+        </Routes>
+    );
 };
 
 export default App;
